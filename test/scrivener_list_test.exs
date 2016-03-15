c38341e1224e62aee3f5dfd9c901fefde1795921
Stephen Moloney <stephenmoloney@live.com>
2016-03-16 00:53:33 +0700

    add tests

--- test/scrivener_list_test.exs
+++ test/scrivener_list_test.exs
@@ -1,8 +1,163 @@
 defmodule ScrivenerListTest do
-  use ExUnit.Case
-  doctest ScrivenerList
+  use ScrivenerList.TestCase
+  @languages ["C#", "C++", "Clojure", "Elixir", "Erlang", "Go", "JAVA", "JavaScript", "Lisp",
+                       "PHP", "Perl", "Python", "Ruby", "Rust", "SQL"]
+  @total_entries length(@languages)
+  @config %Scrivener.Config{page_number: 2, page_size: 4}
 
-  test "the truth" do
-    assert 1 + 1 == 2
+
+  describe "paginate when using a repo module" do
+    it "can paginate a list using a Repo module  - 1" do
+      page = Scrivener.Repo.paginate(@languages)
+
+      assert page.page_size == 5
+      assert page.page_number == 1
+      assert page.entries == ["C#", "C++", "Clojure", "Elixir", "Erlang"]
+      assert page.total_entries == @total_entries
+      assert page.total_pages == Float.ceil(length(@languages)/5)
+    end
+
+    it "can paginate a list using a Repo module - 2" do
+      languages = Enum.take(@languages, 3)
+      page = Scrivener.Repo.paginate(languages)
+
+      assert page.page_number == 1
+      assert page.page_size == 5
+      assert page.entries == ["C#", "C++", "Clojure"]
+      assert page.total_entries == 3
+      assert page.total_pages == Float.ceil(length(languages)/5)
+    end
+
+    it "can paginate a list when provided the current page and page size as a params map" do
+      page = Scrivener.Repo.paginate(@languages, %{"page" => "2", "page_size" => "3"})
+
+      assert page.page_size == 3
+      assert page.page_number == 2
+      assert page.entries == ["Elixir", "Erlang", "Go"]
+      assert page.total_entries == @total_entries
+      assert page.total_pages == Float.ceil(length(@languages)/3)
+    end
+
+    it "can paginate a list when only provided with the page size as a keyword list
+        and the page number defaults to page 1" do
+      page = Scrivener.Repo.paginate(@languages, page_size: 3)
+
+      assert page.page_size == 3
+      assert page.page_number == 1
+      assert page.entries == ["C#", "C++", "Clojure"]
+      assert page.total_pages == Float.ceil(length(@languages)/3)
+      assert page.total_entries == @total_entries
+    end
+
+    it "can paginate a list when only provided with the page number as a keyword list
+        and the page size defaults to the Repo configured page size" do
+      page = Scrivener.Repo.paginate(@languages, page: 2)
+
+      assert page.page_size == 5
+      assert page.page_number == 2
+      assert page.entries == ["Go", "JAVA", "JavaScript", "Lisp", "PHP"]
+      assert page.total_pages == Float.ceil(length(@languages)/5)
+      assert page.total_entries == @total_entries
+    end
+
+    it "can respect a max_page_size configuration" do
+      page = Scrivener.Repo.paginate(@languages, page: 2, page_size: 200)
+
+      assert page.page_size < 200
+      assert page.page_size == 10 # this is the max_page_size set in the Repo `use` statement.
+      assert page.page_number == 2
+      assert page.entries ==  ["Perl", "Python", "Ruby", "Rust", "SQL"]
+      assert page.total_pages == Float.ceil(length(@languages)/10)
+      assert page.total_entries == @total_entries
+    end
   end
+
+
+  describe "paginate without using a repo module" do
+    it "can paginate a list when passed a Scrivener.Config. t directly - 1" do
+      page = Scrivener.Paginater.paginate(@languages, @config)
+
+      assert page.page_number == 2
+      assert page.page_size == 4
+      assert page.entries == ["Erlang", "Go", "JAVA", "JavaScript"]
+      assert page.total_entries == @total_entries
+      assert page.total_pages == Float.ceil(length(@languages)/4)
+    end
+
+    it "can paginate a list when passed a Scrivener.Config. t directly - 2" do
+      languages = Enum.take(@languages, 3)
+      page = Scrivener.Paginater.paginate(languages, @config)
+
+      assert page.page_number == 2
+      assert page.page_size == 4
+      assert page.entries == []
+      assert page.total_entries == 3
+      assert page.total_pages == Float.ceil(length(languages)/4)
+    end
+
+    it "can paginate a list when provided the current page and page size as a params map" do
+      page = ScrivenerList.paginate(@languages, %{"page" => "2", "page_size" => "3"})
+
+      assert page.page_size == 3
+      assert page.page_number == 2
+      assert page.entries == ["Elixir", "Erlang", "Go"]
+      assert page.total_entries == @total_entries
+      assert page.total_pages == Float.ceil(length(@languages)/3)
+    end
+
+    it "can paginate a list when provided the current page and page size as a keyword list" do
+      page = ScrivenerList.paginate(@languages, page: 2, page_size: 3)
+
+      assert page.page_size == 3
+      assert page.page_number == 2
+      assert page.entries == ["Elixir", "Erlang", "Go"]
+      assert page.total_pages == Float.ceil(length(@languages)/3)
+      assert page.total_entries == @total_entries
+    end
+
+
+    it "can paginate a list when only provided with the page size as a keyword list
+        and the page number defaults to page 1" do
+      page = ScrivenerList.paginate(@languages, page_size: 3)
+
+      assert page.page_size == 3
+      assert page.page_number == 1
+      assert page.entries == ["C#", "C++", "Clojure"]
+      assert page.total_pages == Float.ceil(length(@languages)/3)
+      assert page.total_entries == @total_entries
+    end
+
+    it "can paginate a list when only provided with the page number as a keyword list
+        and the page size defaults to 10" do
+      page = ScrivenerList.paginate(@languages, page: 2)
+
+      assert page.page_size == 10
+      assert page.page_number == 2
+      assert page.entries == [ "Perl", "Python", "Ruby", "Rust", "SQL"]
+      assert page.total_pages == Float.ceil(length(@languages)/10)
+      assert page.total_entries == @total_entries
+    end
+
+    it "can paginate a list when provided the current page and page size is absent" do
+      page = ScrivenerList.paginate(@languages, page: 2)
+
+      assert page.page_size == 10
+      assert page.page_number == 2
+      assert page.entries == ["Perl", "Python", "Ruby", "Rust", "SQL"]
+      assert page.total_pages == Float.ceil(length(@languages)/10)
+      assert page.total_entries == @total_entries
+    end
+
+    it "cannot respect a max_page_size configuration" do
+      page = ScrivenerList.paginate(@languages, page: 2, page_size: 200)
+
+      refute page.page_size < 200
+      assert page.page_number == 2
+      assert page.entries == []
+      assert page.total_pages == Float.ceil(length(@languages)/200)
+      assert page.total_entries == @total_entries
+    end
+  end
+
+
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,15 @@
+defmodule ScrivenerList.TestCase do
+  use ExUnit.CaseTemplate
+  using(opts) do
+    quote do
+      use ExSpec, unquote(opts)
+    end
+  end
+end
+
+defmodule Scrivener.Repo do
+  use Ecto.Repo, otp_app: :scrivener_list
+  use Scrivener, page_size: 5, max_page_size: 10
+end
+
 ExUnit.start()
