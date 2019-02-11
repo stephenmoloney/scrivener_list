c627abe5cde6cdb0f5d68369b0b409fe7b2f11ea
Stephen Moloney <stephen@stephenmoloney.com>
2019-02-11 09:48:05 +0000

    Introduce mix format
    What does this commit/MR/PR do ?
    
    - Introduce mix format
    
    Why is this commit/MR/PR needed ?
    
    - Linted/standardised code

--- mix.exs
+++ mix.exs
@@ -7,42 +7,38 @@ defmodule Scrivener.List.Mixfile do
 
   def project do
     [
-     app: :scrivener_list,
-     name: @name,
-     version: @version,
-     source_url: @source_url,
-     homepage_url: @homepage_url,
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     deps: deps(),
-     docs: docs()
+      app: :scrivener_list,
+      name: @name,
+      version: @version,
+      source_url: @source_url,
+      homepage_url: @homepage_url,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs()
     ]
   end
-
 
   def application do
     [applications: [:logger, :scrivener_ecto]]
   end
 
-
   defp deps() do
     [
-     {:scrivener_ecto, "~> 1.0 or ~> 2.0"},
-     {:earmark, "~> 1.3", only: :dev},
-     {:ex_doc, "~> 0.19", only: :dev}
+      {:scrivener_ecto, "~> 1.0 or ~> 2.0"},
+      {:earmark, "~> 1.3", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev}
     ]
   end
-
 
   defp description() do
     ~S"""
     A scrivener/scrivener_ecto compatible extension that allows pagination of a list.
     """
   end
-
 
   defp package() do
     [
@@ -59,13 +55,11 @@ defmodule Scrivener.List.Mixfile do
 
   defp docs do
     [
-     main: "README.md",
-     extra_section: "GUIDE",
-     extras: [
-              "README.md": [path: "README.md", title: "GUIDE"]
-             ]
+      main: "README.md",
+      extra_section: "GUIDE",
+      extras: [
+        "README.md": [path: "README.md", title: "GUIDE"]
+      ]
     ]
   end
-
-
 end
